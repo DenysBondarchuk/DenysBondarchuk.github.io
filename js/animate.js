756179e065c1afa0b78a2fd81c8b419cffc9a48b
DenysBondarchuk <bondarchuk520@gmail.com>
2017-05-31 18:16:51 +0300

    Strict

---
+++ js/animate.js
@@ -0,0 +1,7 @@
+$(window).load(function(){
+	$("header img").animated("fadeInRightBig","fadeInRightBig");
+	$(".banner h1").animated("fadeInDown","fadeOutDown");
+	$(".feature .icon1").animated("bounceInLeft","bounceOutLeft");
+	$(".feature .icon2").animated("bounceIn","bounceOut");
+	$(".feature .icon3").animated("bounceInRight","bounceOutRight");
+});
