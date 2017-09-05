856ca14fd38795c28652eb606710edc9ac37c438
DenysBondarchuk <bondarchuk520@gmail.com>
2017-09-05 19:18:08 +0300

    update for strict

--- strict/js/animate.js
+++ strict/js/animate.js
@@ -1,7 +1,7 @@
 $(window).load(function(){
 	$("header img").animated("fadeInRightBig","fadeInRightBig");
-	$(".banner h1").animated("fadeInDown","fadeOutDown");
-	$(".feature .icon1").animated("bounceInLeft","bounceOutLeft");
-	$(".feature .icon2").animated("bounceIn","bounceOut");
-	$(".feature .icon3").animated("bounceInRight","bounceOutRight");
+	$(".banner h1").animated("fadeInDown","fadeInDown");
+	$(".feature .icon1").animated("slideInLeft","fadeOut");
+	$(".feature .icon2").animated("bounceIn","fadeOut");
+	$(".feature .icon3").animated("slideInRight","fadeOut");
 });
