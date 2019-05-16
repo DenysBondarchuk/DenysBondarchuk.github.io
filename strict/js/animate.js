707b8a3169cf8a52b8ed4f48a6e58ebaf1de8a2f
DenysBondarchuk <bondarchuk520@gmail.com>
2019-05-16 09:45:19 +0300

    add new portfolio v2.0

--- strict/js/animate.js
+++
@@ -1,7 +0,0 @@
-$(window).load(function(){
-	$("header img").animated("fadeInRightBig","fadeInRightBig");
-	$(".banner h1").animated("fadeInDown","fadeInDown");
-	$(".feature .icon1").animated("slideInLeft","fadeOut");
-	$(".feature .icon2").animated("bounceIn","fadeOut");
-	$(".feature .icon3").animated("slideInRight","fadeOut");
-});
